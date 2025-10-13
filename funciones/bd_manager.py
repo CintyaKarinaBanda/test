79f1a2e129352985a5c8027c37986c5e493411f3
Karina-Banda <karina.banda@xaldigital.com>
2025-10-13 11:49:02 -0600

    idk

--- funciones/bd_manager.py
+++ funciones/bd_manager.py
@@ -1,9 +1,7 @@
 import psycopg2
 from datetime import date
-import sys
 import os
-sys.path.append(os.path.dirname(__file__))
-from config import host, database, user, password
+from funciones.config import host, database, user, password
 
 def send_status(comments, cuenta, project):
     comments_texto = '\n'.join(comments) if isinstance(comments, list) else comments
