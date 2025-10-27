d5e98773fecb499d2e0321d3df61a75ab005a980
CintyaKarinaBanda <karibanda10@gmail.com>
2025-10-27 13:46:55 -0600

    Sisamex Changes

--- sisamex/Script.py
+++ sisamex/Script.py
@@ -12,13 +12,19 @@ from config import REMITENTE, GMAIL_PASSWORD, DESTINATARIO, COPIAS, SUBJECT, CUE
 
 if __name__ == "__main__":
     print("Script iniciado, ",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    #Actualizacion en Xoc
-    send_status(["Todo Ok"], CUENTA, PROJECT, host, database, user, password)
-    
-    parametros = {
-        "Chats": ("Actividad en el chat", "Ok", "Nok")
-    }
-    #Envio de correo
-    #send_email(None, ['Todo Ok'], REMITENTE, GMAIL_PASSWORD, DESTINATARIO, COPIAS, SUBJECT, parametros)
+    monthList = [5, 6, 7]
+    if (datetime.now().month() not in monthList):
+        #Actualizacion en Xoc
+        send_status(["Todo Ok"], CUENTA, PROJECT, host, database, user, password)
+        #Actualizacion en Xoc
+    else:
+        #Actualizacion en Xoc
+        send_status(["Todo Ok"], CUENTA, PROJECT, host, database, user, password)
+        
+        parametros = {
+            "Chats": ("Actividad en el chat", "Ok", "Nok")
+        }
+        #Envio de correo
+        #send_email(None, ['Todo Ok'], REMITENTE, GMAIL_PASSWORD, DESTINATARIO, COPIAS, SUBJECT, parametros)
 
-    print("Script terminado, ",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        print("Script terminado, ",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
