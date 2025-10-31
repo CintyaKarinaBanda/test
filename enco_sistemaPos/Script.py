88a10d40087088a30751131ad784fffe21e20a64
CintyaKarinaBanda <karibanda10@gmail.com>
2025-10-31 10:02:26 -0600

    Rediseño login - Mejoras modulo Ble

--- enco_sistemaPos/Script.py
+++ enco_sistemaPos/Script.py
@@ -2,18 +2,22 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'funciones'))
 
-import shutil
+
 from datetime import datetime
 
 #Funciones extraidas dentro del mismo proyecto
 from bd_manager import send_status
 from api_manager import verify_page
+from email_manager import send_email
 from config import API_LINK, REMITENTE, GMAIL_PASSWORD, DESTINATARIO, COPIAS, SUBJECT, CUENTA, PROJECT, host, database, user, password
 
 if __name__ == "__main__":
     print("Script iniciado, ",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     api_availability = verify_page(API_LINK)
+    
+    # Generar comentarios basado en disponibilidad de API
+    comentarios = "Todo Ok" if api_availability else "API no disponible"
 
     #Actualizacion en Xoc
     send_status(comentarios, CUENTA, PROJECT, host, database, user, password)
